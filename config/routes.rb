b0853e70f506d9b8a3827065d668b6f0045943f5
ippeiman <hawaippei6@gmail.com>
2023-04-13 20:24:19 +0900

    messageテーブルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-<<<<<<< Updated upstream
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
 end
-=======
-  root to: "messages#index"
-  resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
-end
->>>>>>> Stashed changes
